f5c472e2adf34c80e36d92fc793d4aaaa21fd027
nateusse <natyeusse@gmail.com>
2025-03-11 12:42:39 -0400

    initial files
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/microserviceshouses/MicroservicesHousesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserviceshouses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesHousesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
